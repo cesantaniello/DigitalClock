c14dc70f13223b3de55fc395cbb6d48dac69ba41
Administrator <admin@example.com>
2021-08-25 16:43:52 +0200

    4. Add formatHour function

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,21 @@
 const showClock = () => {
     let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let hours = formatHour(date.getHours());
+    let minutes = formatHour(date.getMinutes());
+    let seconds = formatHour(date.getSeconds());
     document.getElementById("hour").innerHTML = `${hours}:${minutes}:${seconds}`;
+
+    const months = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let dateString = `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    document.getElementById("date").innerHTML = dateString;
+}
+
+const formatHour = (hour) => {
+    if(hour < 10)
+        hour = '0' + hour;
+    return hour;
 }
 
 setInterval(showClock, 1000);
